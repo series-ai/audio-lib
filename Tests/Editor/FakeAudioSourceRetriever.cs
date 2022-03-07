81ec97cfeb0fb16b20dafe86061be4c3956fc8bc
Maxi <maximuller11@gmail.com>
2022-03-06 21:46:38 -0300

    AudioManager tests

---
+++ Tests/Editor/FakeAudioSourceRetriever.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Padoru.Audio.Tests
+{
+    public class FakeAudioSourceRetriever : IAudioSourceRetriever
+    {
+        public AudioSource GetAudio()
+        {
+            return null;
+        }
+
+        public void ReturnAudio(AudioSource audioSource)
+        {
+
+        }
+    }
+}
