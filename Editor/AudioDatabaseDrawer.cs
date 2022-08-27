cdfc52998de96258d935cec51447c544c20aeafe
Maxi <maximuller11@gmail.com>
2022-08-27 17:28:21 -0300

    AudioDatabaseEditor renamed to AudioDatabaseDrawer

--- Editor/AudioDatabaseDrawer.cs
+++ Editor/AudioDatabaseDrawer.cs
@@ -1,8 +1,6 @@
 using Padoru.Core.Editor;
 using UnityEditor;
 
-using Debug = Padoru.Diagnostics.Debug;
-
 namespace Padoru.Audio.Editor
 {
 	[CustomPropertyDrawer(typeof(AudioDatabaseDictionary))]
